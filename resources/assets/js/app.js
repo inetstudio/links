44351619e4c554589eaee7dd8cd4eea15a55a402
Konstantin Bukin <bukin@inetstudio.ru>
2018-12-20 19:55:33 +0300

    Fixes for laravel mix update

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,15 +2,11 @@ require('./plugins/tinymce/plugins/links');
 
 require('./stores/links');
 
-Vue.component('LinksList', function (resolve) {
-    require(['./components/partials/LinksList/LinksList.vue'], resolve);
-});
-Vue.component('LinksListItem', function (resolve) {
-    require(['./components/partials/LinksList/LinksListItem.vue'], resolve);
-});
-Vue.component('LinksListItemForm', function (resolve) {
-    require(['./components/partials/LinksList/LinksListItemForm.vue'], resolve);
-});
+require('../../../../widgets/resources/assets/js/mixins/widget');
+
+Vue.component('LinksList', require('./components/partials/LinksList/LinksList.vue').default);
+Vue.component('LinksListItem', require('./components/partials/LinksList/LinksListItem.vue').default);
+Vue.component('LinksListItemForm', require('./components/partials/LinksList/LinksListItemForm.vue').default);
 
 let links = require('./package/links');
 links.init();
